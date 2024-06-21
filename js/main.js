03997b02c4fdb8458bd389f657c16969ea32f48d
Oliur Rahman <62242483+oliur-portfolio@users.noreply.github.com>
2024-06-22 04:07:52 +0600

    complete page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+// Copy Clipboard plugin
+document.addEventListener("DOMContentLoaded", (event) => {
+  const clipboard = new ClipboardJS(".copy-button");
+
+  clipboard.on("success", function (e) {
+    toastr.success("Text copied to clipboard!");
+    e.clearSelection();
+  });
+
+  clipboard.on("error", function (e) {
+    toastr.error("Failed to copy text.");
+  });
+});
+
 //FAQ accordion Effect
 $(".accordion__question").click(function (e) {
   e.preventDefault();
